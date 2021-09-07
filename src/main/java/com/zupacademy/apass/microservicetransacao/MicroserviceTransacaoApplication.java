1753435b1fc6fb0c9a500b1ec4a0bd816628a410
Anderson Passos Silva <anderson.silva@zup.com.br>
2021-09-07 12:06:28 -0300

    Inicializa projeto

---
+++ src/main/java/com/zupacademy/apass/microservicetransacao/MicroserviceTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.zupacademy.apass.microservicetransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zupacademy/apass/microservicetransacao/MicroserviceTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupacademy.apass.microservicetransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
